e1adbc7970983253fcb05b77324b1321db5b4c8c
Michael Aaron Murphy <mmstick@pm.me>
2022-02-27 14:49:33 +0100

    fix: Renice all threads of a process when adjusting priorities

--- src/nice.rs
+++ src/nice.rs
@@ -1,8 +1,24 @@
 // Copyright 2022 System76 <info@system76.com>
 // SPDX-License-Identifier: MPL-2.0
 
+use concat_in_place::strcat;
+
 pub fn set_priority(process: u32, priority: i32) {
-    unsafe {
-        libc::setpriority(libc::PRIO_PROCESS, process, priority);
+    let mut buffer = itoa::Buffer::new();
+    let tasks = strcat!("/proc/" buffer.format(process) "/task");
+
+    if let Ok(tasks) = std::fs::read_dir(tasks) {
+        for task in tasks.filter_map(Result::ok) {
+            if let Some(process) = task
+                .file_name()
+                .to_str()
+                .and_then(|num| num.parse::<u32>().ok())
+            {
+                tracing::debug!("set_priority {}: {}", process, priority);
+                unsafe {
+                    libc::setpriority(libc::PRIO_PROCESS, process, priority);
+                }
+            }
+        }
     }
 }
